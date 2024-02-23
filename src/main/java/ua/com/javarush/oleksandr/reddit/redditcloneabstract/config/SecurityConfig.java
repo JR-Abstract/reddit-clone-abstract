95ba62a0e974706f8fbb97763f81c6181a7ab09f
oleksandr-jr <oleksandr.jr@gmail.com>
2024-02-23 21:12:42 +0100

    Disable security

---
+++ src/main/java/ua/com/javarush/oleksandr/reddit/redditcloneabstract/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package ua.com.javarush.oleksandr.reddit.redditcloneabstract.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> {
+                    request.anyRequest().permitAll();
+                }).build();
+    }
+}
