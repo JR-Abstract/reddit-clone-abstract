b55ea8d5a9854fab427eef0bddcc3cec4e3008bc
oleksandr-jr <oleksandr.jr@gmail.com>
2024-02-23 21:10:02 +0100

    Init

---
+++ src/main/java/ua/com/javarush/oleksandr/reddit/redditcloneabstract/RedditCloneAbstractApplication.java
@@ -0,0 +1,13 @@
+package ua.com.javarush.oleksandr.reddit.redditcloneabstract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditCloneAbstractApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedditCloneAbstractApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/com/javarush/oleksandr/reddit/redditcloneabstract/RedditCloneAbstractApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.javarush.oleksandr.reddit.redditcloneabstract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditCloneAbstractApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
