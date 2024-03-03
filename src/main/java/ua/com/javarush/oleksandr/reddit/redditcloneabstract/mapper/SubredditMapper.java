7a0d55b2dd75b8e50d953c35718f2135ba5b7c98
oleksandr-jr <oleksandr.cho@gmail.com>
2024-03-03 14:38:22 +0100

    Some simplification for the SubredditMapper.

--- src/main/java/ua/com/javarush/oleksandr/reddit/redditcloneabstract/mapper/SubredditMapper.java
+++ src/main/java/ua/com/javarush/oleksandr/reddit/redditcloneabstract/mapper/SubredditMapper.java
@@ -10,39 +10,33 @@ import ua.com.javarush.oleksandr.reddit.redditcloneabstract.model.Subreddit;
 import ua.com.javarush.oleksandr.reddit.redditcloneabstract.model.User;
 import ua.com.javarush.oleksandr.reddit.redditcloneabstract.service.PostService;
 import ua.com.javarush.oleksandr.reddit.redditcloneabstract.service.UserService;
-
 @Mapper(componentModel = "spring")
 public abstract class SubredditMapper {
 
-    private PostService postService;
+    @Autowired
     private UserService userService;
 
-    @Mapping(source = "id", target = "id")
-    @Mapping(source = "name", target = "name")
-    @Mapping(source = "description", target = "description")
-    @Mapping(source = "id", target = "numberOfPosts", qualifiedByName = "getCountSubredditPosts")
-    abstract public SubredditResponseDTO subredditToSubredditResponseDto(Subreddit subreddit);
+    @Autowired
+    private PostService postService;
+
+    @Mapping(target = "numberOfPosts", source = "id", qualifiedByName = "getCountSubredditPosts")
+    public abstract SubredditResponseDTO subredditToSubredditResponseDto(Subreddit subreddit);
 
     @Mapping(target = "id", ignore = true)
-    @Mapping(source = "name", target = "name")
-    @Mapping(source = "description", target = "description")
     @Mapping(target = "createdDate", ignore = true)
-    @Mapping(source = "username", target = "user", qualifiedByName = "getUserByUsername")
-    abstract public Subreddit subredditRequestDtoToSubreddit(SubredditRequestDTO subredditRequestDTO);
+    @Mapping(target = "user", source = "username", qualifiedByName = "getUserByUsername")
+    public abstract Subreddit subredditRequestDtoToSubreddit(SubredditRequestDTO subredditRequestDTO);
 
-    @Named("getCountSubredditPosts")
+    @Named(value = "getCountSubredditPosts")
     protected Integer getCountSubredditPosts(Long id) {
         return postService.countAllBySubreddit_Id(id);
     }
 
-    @Named("getUserByUsername")
+    @Named(value = "getUserByUsername")
     protected User getUserByUsername(String username) {
         return userService.findUserByUsername(username);
     }
-
-    @Autowired
-    protected void setPostService(PostService postService, UserService userService) {
-        this.postService = postService;
-        this.userService = userService;
-    }
 }
+
+
+
