76da08672a8cb262abdad0b8c3bdc2d8a246ca61
Andrii Chipets <andrii.chipets@gmail.com>
2024-03-09 15:57:06 +0200

    added all db tables to test schema.sql file

--- src/test/resources/sql/schema.sql
+++ src/test/resources/sql/schema.sql
@@ -1,3 +1,5 @@
+-- Db user
+
 DROP ROLE IF EXISTS abstract_reddit_user;
 
 CREATE ROLE abstract_reddit_user WITH
@@ -10,24 +12,144 @@ CREATE ROLE abstract_reddit_user WITH
     PASSWORD '1234';
 
 -- Drop all tables
-DROP TABLE IF EXISTS public."user" CASCADE;
+DROP TABLE IF EXISTS "user" CASCADE;
+DROP TABLE IF EXISTS refresh_token CASCADE;
+DROP TABLE IF EXISTS subreddit CASCADE;
+DROP TABLE IF EXISTS post CASCADE;
+DROP TABLE IF EXISTS comment CASCADE;
+DROP TABLE IF EXISTS subreddit_posts CASCADE;
+DROP TABLE IF EXISTS token CASCADE;
+DROP TABLE IF EXISTS vote CASCADE;
 
--- SCHEMA: abstract_reddit_schema
+-- SCHEMA: public
 DROP SCHEMA IF EXISTS public;
 CREATE SCHEMA IF NOT EXISTS public
     AUTHORIZATION abstract_reddit_user;
 
-CREATE TABLE IF NOT EXISTS public."user"
+-- Tables
+CREATE TABLE IF NOT EXISTS "user"
 (
-    id BIGSERIAL not null
-        constraint user_pk
-            primary key,
-    created  timestamp,
-    email    varchar(255),
-    enabled  boolean,
-    password varchar(255),
-    username varchar(255)
+    id       BIGSERIAL NOT NULL
+        CONSTRAINT user_pk
+            PRIMARY KEY,
+    created  TIMESTAMP,
+    email    VARCHAR(255),
+    enabled  BOOLEAN,
+    password VARCHAR(255),
+    username VARCHAR(255)
 );
 
-alter table public."user"
-    owner to abstract_reddit_user;
+ALTER TABLE "user"
+    OWNER TO abstract_reddit_user;
+
+CREATE TABLE IF NOT EXISTS refresh_token
+(
+    id           BIGSERIAL NOT NULL
+        CONSTRAINT refresh_token_pk
+            PRIMARY KEY,
+    created_date TIMESTAMP,
+    token        VARCHAR(255)
+);
+
+ALTER TABLE refresh_token
+    OWNER TO abstract_reddit_user;
+
+CREATE TABLE IF NOT EXISTS subreddit
+(
+    id           BIGSERIAL NOT NULL
+        CONSTRAINT subreddit_pk
+            PRIMARY KEY,
+    created_date TIMESTAMP,
+    description  VARCHAR(255),
+    name         VARCHAR(255),
+    user_id      BIGINT
+        CONSTRAINT subreddit_user_id_fk
+            REFERENCES "user"
+);
+
+ALTER TABLE subreddit
+    OWNER TO abstract_reddit_user;
+
+CREATE TABLE IF NOT EXISTS post
+(
+    id           BIGSERIAL NOT NULL
+        CONSTRAINT post_pk
+            PRIMARY KEY,
+    created_date TIMESTAMP,
+    description  VARCHAR(255),
+    post_name    VARCHAR(255),
+    url          VARCHAR(255),
+    vote_count   INTEGER,
+    subreddit_id BIGINT
+        CONSTRAINT post_subreddit_id_fk
+            REFERENCES subreddit,
+    user_id      BIGINT
+        CONSTRAINT post___fk
+            REFERENCES "user"
+);
+
+ALTER TABLE post
+    OWNER TO abstract_reddit_user;
+
+CREATE TABLE IF NOT EXISTS comment
+(
+    id           BIGSERIAL NOT NULL
+        CONSTRAINT comment_pk
+            PRIMARY KEY,
+    created_date TIMESTAMP,
+    text         VARCHAR(255),
+    post_id      BIGINT
+        CONSTRAINT comment_post_id_fk
+            REFERENCES post,
+    user_id      BIGINT
+        CONSTRAINT comment_user_id_fk
+            REFERENCES "user"
+);
+
+ALTER TABLE comment
+    OWNER TO abstract_reddit_user;
+
+CREATE TABLE IF NOT EXISTS subreddit_posts
+(
+    subreddit_id BIGSERIAL NOT NULL
+        CONSTRAINT subreddit_posts_subreddit_id_fk
+            REFERENCES subreddit,
+    posts_id     BIGINT
+        CONSTRAINT subreddit_posts_post_id_fk
+            REFERENCES post
+);
+
+ALTER TABLE subreddit_posts
+    OWNER TO abstract_reddit_user;
+
+CREATE TABLE IF NOT EXISTS token
+(
+    id          BIGSERIAL NOT NULL
+        CONSTRAINT token_pk
+            PRIMARY KEY,
+    expiry_date TIMESTAMP,
+    token       VARCHAR(255),
+    user_id     BIGINT
+        CONSTRAINT token_user_id_fk
+            REFERENCES "user"
+);
+
+ALTER TABLE token
+    OWNER TO abstract_reddit_user;
+
+CREATE TABLE IF NOT EXISTS vote
+(
+    id        BIGSERIAL NOT NULL
+        CONSTRAINT vote_pk
+            PRIMARY KEY,
+    vote_type SMALLINT,
+    post_id   BIGINT
+        CONSTRAINT vote_post_id_fk
+            REFERENCES post,
+    user_id   BIGINT
+        CONSTRAINT vote_user_id_fk
+            REFERENCES "user"
+);
+
+ALTER TABLE vote
+    OWNER TO abstract_reddit_user;
