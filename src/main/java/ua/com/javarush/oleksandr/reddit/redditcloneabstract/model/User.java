2a7f252b15e93e8a629478596b3c2ef525c150f7
Oleksandr Havriush <havryush@gmail.com>
2024-03-10 21:39:09 +0100

    Add annotations to fields of User class using jakarta.validation.constraints.*

--- src/main/java/ua/com/javarush/oleksandr/reddit/redditcloneabstract/model/User.java
+++ src/main/java/ua/com/javarush/oleksandr/reddit/redditcloneabstract/model/User.java
@@ -1,6 +1,7 @@
 package ua.com.javarush.oleksandr.reddit.redditcloneabstract.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,16 +23,22 @@ public class User {
     @Column(name = "id")
     private Long userId;
 
+    @Email
+    @NotBlank(message = "Email is mandatory")
     private String email;
 
+    @NotBlank(message = "Username is mandatory")
+    @Size(min = 2, max = 30, message = "Username must be between 2 and 30 characters")
     private String username;
 
+    @NotBlank(message = "Password is mandatory")
+    @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
 
     private boolean enabled;
 
     @CreationTimestamp
-    @Column(name = "created",  nullable = false, updatable = false, columnDefinition = "DATETIME(6)")
+    @Column(name = "created", nullable = false, updatable = false, columnDefinition = "DATETIME(6)")
     private ZonedDateTime created_at;
 
 }
