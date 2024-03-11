aa757eb1af759a9f2c52354adc992c642608f52a
Aleksei Os. <oal.aleos@gmail.com>
2024-03-11 14:07:46 +0200

    Add 'role' and 'user_role' tables to postgres DDL
    The postgres DDL now includes two new tables, 'role' and 'user_role', to better manage user roles and permissions. Meanwhile, the existing tables have been reformatted slightly for improved readability, but their structure and roles remain unchanged.
    
    Signed-off-by: Aleksei Os. <oal.aleos@gmail.com>

--- src/main/resources/ignore/postgres_DDL.sql
+++ src/main/resources/ignore/postgres_DDL.sql
@@ -1,126 +1,143 @@
-create table if not exists refresh_token
+CREATE TABLE IF NOT EXISTS refresh_token
 (
-    id           BIGSERIAL not null
-    constraint refresh_token_pk
-    primary key,
+    id           BIGSERIAL NOT NULL
+        CONSTRAINT refresh_token_pk
+            PRIMARY KEY,
     created_date timestamp,
     token        varchar(255)
-    );
+);
 
-alter table refresh_token
-    owner to root;
+ALTER TABLE refresh_token
+    OWNER TO root;
 
-create table if not exists "user"
+CREATE TABLE IF NOT EXISTS "user"
 (
-    id       BIGSERIAL not null
-    constraint user_pk
-    primary key,
+    id       BIGSERIAL NOT NULL
+        CONSTRAINT user_pk
+            PRIMARY KEY,
     created  timestamp,
     email    varchar(255),
     enabled  boolean,
     password varchar(255),
     username varchar(255)
-    );
+);
 
-alter table "user"
-    owner to root;
+ALTER TABLE "user"
+    OWNER TO root;
 
-create table if not exists subreddit
+CREATE TABLE IF NOT EXISTS subreddit
 (
-    id           BIGSERIAL not null
-    constraint subreddit_pk
-    primary key,
+    id           BIGSERIAL NOT NULL
+        CONSTRAINT subreddit_pk
+            PRIMARY KEY,
     created_date timestamp,
     description  varchar(255),
     name         varchar(255),
     user_id      bigint
-    constraint subreddit_user_id_fk
-    references "user"
-    );
+        CONSTRAINT subreddit_user_id_fk
+            REFERENCES "user"
+);
 
-alter table subreddit
-    owner to root;
+ALTER TABLE subreddit
+    OWNER TO root;
 
-create table if not exists post
+CREATE TABLE IF NOT EXISTS post
 (
-    id           BIGSERIAL not null
-    constraint post_pk
-    primary key,
+    id           BIGSERIAL NOT NULL
+        CONSTRAINT post_pk
+            PRIMARY KEY,
     created_date timestamp,
     description  varchar(255),
     post_name    varchar(255),
     url          varchar(255),
     vote_count   integer,
     subreddit_id bigint
-    constraint post_subreddit_id_fk
-    references subreddit,
+        CONSTRAINT post_subreddit_id_fk
+            REFERENCES subreddit,
     user_id      bigint
-    constraint post___fk
-    references "user"
-    );
+        CONSTRAINT post___fk
+            REFERENCES "user"
+);
 
-alter table post
-    owner to root;
+ALTER TABLE post
+    OWNER TO root;
 
-create table if not exists comment
+CREATE TABLE IF NOT EXISTS comment
 (
-    id           BIGSERIAL not null
-    constraint comment_pk
-    primary key,
+    id           BIGSERIAL NOT NULL
+        CONSTRAINT comment_pk
+            PRIMARY KEY,
     created_date timestamp,
     text         varchar(255),
     post_id      bigint
-    constraint comment_post_id_fk
-    references post,
+        CONSTRAINT comment_post_id_fk
+            REFERENCES post,
     user_id      bigint
-    constraint comment_user_id_fk
-    references "user"
-    );
-
-alter table comment
-    owner to root;
-
-create table if not exists subreddit_posts
-(
-    subreddit_id BIGSERIAL not null
-    constraint subreddit_posts_subreddit_id_fk
-    references subreddit,
-    posts_id     bigint
-    constraint subreddit_posts_post_id_fk
-    references post
+        CONSTRAINT comment_user_id_fk
+            REFERENCES "user"
 );
 
-alter table subreddit_posts
-    owner to root;
+ALTER TABLE comment
+    OWNER TO root;
 
-create table if not exists token
+CREATE TABLE IF NOT EXISTS subreddit_posts
 (
-    id          BIGSERIAL not null
-    constraint token_pk
-    primary key,
+    subreddit_id BIGSERIAL NOT NULL
+        CONSTRAINT subreddit_posts_subreddit_id_fk
+            REFERENCES subreddit,
+    posts_id     bigint
+        CONSTRAINT subreddit_posts_post_id_fk
+            REFERENCES post
+);
+
+ALTER TABLE subreddit_posts
+    OWNER TO root;
+
+CREATE TABLE IF NOT EXISTS token
+(
+    id          BIGSERIAL NOT NULL
+        CONSTRAINT token_pk
+            PRIMARY KEY,
     expiry_date timestamp,
     token       varchar(255),
     user_id     bigint
-    constraint token_user_id_fk
-    references "user"
-    );
-
-alter table token
-    owner to root;
-
-create table if not exists vote
-(
-    id        BIGSERIAL not null
-    constraint vote_pk
-    primary key,
-    vote_type smallint,
-    post_id   bigint
-    constraint vote_post_id_fk
-    references post,
-    user_id   bigint
-    constraint vote_user_id_fk
-    references "user"
+        CONSTRAINT token_user_id_fk
+            REFERENCES "user"
 );
 
-alter table vote
-    owner to root;
+ALTER TABLE token
+    OWNER TO root;
+
+CREATE TABLE IF NOT EXISTS vote
+(
+    id        BIGSERIAL NOT NULL
+        CONSTRAINT vote_pk
+            PRIMARY KEY,
+    vote_type smallint,
+    post_id   bigint
+        CONSTRAINT vote_post_id_fk
+            REFERENCES post,
+    user_id   bigint
+        CONSTRAINT vote_user_id_fk
+            REFERENCES "user"
+);
+
+ALTER TABLE vote
+    OWNER TO root;
+
+CREATE TABLE IF NOT EXISTS role
+(
+    id   BIGSERIAL,
+    name VARCHAR(255),
+    CONSTRAINT "role_pk" PRIMARY KEY (id),
+    CONSTRAINT "uq_role_name" UNIQUE (name)
+);
+
+CREATE TABLE IF NOT EXISTS user_role
+(
+    user_id BIGSERIAL,
+    role_id BIGSERIAL,
+    CONSTRAINT "users_roles_pk" PRIMARY KEY (user_id, role_id),
+    CONSTRAINT "user_role_user_id_fk" FOREIGN KEY (user_id) REFERENCES "user" (id),
+    CONSTRAINT "user_role_role_id_fk" FOREIGN KEY (role_id) REFERENCES "user" (id)
+);
