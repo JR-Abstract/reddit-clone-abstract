60f0fb08cb6654a6212b3f137c54c65f5e00f45c
”Opanasenko-Mykhailo <opanasenko.misha@gmail.com>
2024-03-11 23:29:00 +0200

    done

--- src/main/java/ua/com/javarush/oleksandr/reddit/redditcloneabstract/controller/AuthController.java
+++ src/main/java/ua/com/javarush/oleksandr/reddit/redditcloneabstract/controller/AuthController.java
@@ -1,10 +1,8 @@
 package ua.com.javarush.oleksandr.reddit.redditcloneabstract.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import ua.com.javarush.oleksandr.reddit.redditcloneabstract.dto.LoginRequest;
 import ua.com.javarush.oleksandr.reddit.redditcloneabstract.dto.RegisterRequest;
