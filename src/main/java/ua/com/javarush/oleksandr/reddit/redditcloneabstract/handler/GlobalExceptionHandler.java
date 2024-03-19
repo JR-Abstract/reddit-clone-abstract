cc49f33e6d1c3b606f91491eb1cfc59d8300b821
Aleksei Os. <oal.aleos@gmail.com>
2024-03-19 16:23:29 +0200

    Refactor global exception handling
    Centralized exception handling for user-related exceptions and improved readability by utilizing a base method for constructing error responses. Added new exception classes to the handler and used @Value to externalize error messages.
    
    Signed-off-by: Aleksei Os. <oal.aleos@gmail.com>

--- src/main/java/ua/com/javarush/oleksandr/reddit/redditcloneabstract/handler/GlobalExceptionHandler.java
+++ src/main/java/ua/com/javarush/oleksandr/reddit/redditcloneabstract/handler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package ua.com.javarush.oleksandr.reddit.redditcloneabstract.handler;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
@@ -10,14 +11,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ua.com.javarush.oleksandr.reddit.redditcloneabstract.error.ErrorEntity;
 import ua.com.javarush.oleksandr.reddit.redditcloneabstract.error.SimpleErrorEntity;
-import ua.com.javarush.oleksandr.reddit.redditcloneabstract.exception.PostCreationException;
-import ua.com.javarush.oleksandr.reddit.redditcloneabstract.exception.PostNotFoundException;
-import ua.com.javarush.oleksandr.reddit.redditcloneabstract.exception.SubredditCreateException;
+import ua.com.javarush.oleksandr.reddit.redditcloneabstract.exception.*;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 @RequiredArgsConstructor
 @Slf4j
 public class GlobalExceptionHandler {
+
+    @Value("${subreddit.create.exception}")
+    private String subredditCreateError;
 
     private final MessageSource messageSource;
 
@@ -37,13 +43,42 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Object> handleException(SubredditCreateException e) {
 
         var errorInfo = new ErrorEntity(
-                messageSource.getMessage("subreddit.create.exception", null, LocaleContextHolder.getLocale()),
+                messageSource.getMessage(subredditCreateError, null, LocaleContextHolder.getLocale()),
                 e.getErrors(),
                 System.currentTimeMillis()
         );
 
-        log.error("log.subreddit.create.exception" + errorInfo);
+        log.error(errorInfo.toString());
 
         return new ResponseEntity<>(errorInfo, HttpStatus.BAD_REQUEST);
+    }
+
+    // todo: check possibility to handle response exception with JwtAuthEntryPoint
+
+    @ExceptionHandler(UserDisabledException.class)
+    public ResponseEntity<Object> handleUserDisabledException(UserDisabledException ex) {
+        return buildErrorResponseEntity(HttpStatus.FORBIDDEN, ex);
+    }
+
+    @ExceptionHandler(InvalidPasswordException.class)
+    public ResponseEntity<Object> handleInvalidPasswordException(InvalidPasswordException ex) {
+        return buildErrorResponseEntity(HttpStatus.BAD_REQUEST, ex);
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<Object> handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
+        return buildErrorResponseEntity(HttpStatus.CONFLICT, ex);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException ex) {
+        return buildErrorResponseEntity(HttpStatus.NOT_FOUND, ex);
+    }
+
+    private ResponseEntity<Object> buildErrorResponseEntity(HttpStatus status, RuntimeException ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", ex.getMessage());
+        return new ResponseEntity<>(body, status);
     }
 }
