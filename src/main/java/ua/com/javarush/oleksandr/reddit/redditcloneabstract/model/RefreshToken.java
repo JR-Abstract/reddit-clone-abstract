3e49bce476688fe34ce7857235204880e9940a2d
Aleksei Os. <oal.aleos@gmail.com>
2024-03-24 13:55:54 +0200

    Add refresh token handling
    This commit introduces new functionality for handling refresh tokens in the system. It includes a new RefreshRequest data transfer object (DTO) and RefreshTokenService. The RefreshToken model and repository have also been updated to support related operations.
    
    Signed-off-by: Aleksei Os. <oal.aleos@gmail.com>

--- src/main/java/ua/com/javarush/oleksandr/reddit/redditcloneabstract/model/RefreshToken.java
+++ src/main/java/ua/com/javarush/oleksandr/reddit/redditcloneabstract/model/RefreshToken.java
@@ -3,10 +3,7 @@ package ua.com.javarush.oleksandr.reddit.redditcloneabstract.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 import java.sql.Timestamp;
 
@@ -17,6 +14,7 @@ import java.sql.Timestamp;
 @Builder(builderMethodName = "with")
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class RefreshToken {
 
     @Id
--- src/main/java/ua/com/javarush/oleksandr/reddit/redditcloneabstract/repository/RefreshTokenRepository.java
+++ src/main/java/ua/com/javarush/oleksandr/reddit/redditcloneabstract/repository/RefreshTokenRepository.java
@@ -1,7 +1,15 @@
 package ua.com.javarush.oleksandr.reddit.redditcloneabstract.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ua.com.javarush.oleksandr.reddit.redditcloneabstract.model.RefreshToken;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    void deleteAllByUserEmail(String email);
+
+    @Query("select rt from RefreshToken rt join fetch rt.user where rt.value = :token")
+    Optional<RefreshToken> findByValue(String token);
 }
